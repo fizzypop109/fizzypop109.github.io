5ed4a961395d3596f72904ad209a01d406173188
Kyla Burgess <kyla_burgess@outlook.com>
2020-03-28 17:19:40 +1030

    fossil prices fixed

--- fossils_json.js
+++ fossils_json.js
@@ -2,333 +2,333 @@ var fossils_json =
     [
         {
             "name": "rex skull",
-            "price": 24000
+            "price": 6000
         },
         {
             "name": "rex torso",
-            "price": 22000
+            "price": 5500
         },
         {
             "name": "rex tail",
-            "price": 20000
+            "price": 5000
         },
         {
             "name": "tricera skull",
-            "price": 22000
+            "price": 6000
         },
         {
             
             "name": "tricera torso",
-            "price": 20000
+            "price": 5500
         },
         {
             
             "name": "tricera tail",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "mammoth skull",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "mammoth torso",
-            "price": 10000
+            "price": 2500
         },
         {
             
             "name": "ankylo skull",
-            "price": 14000
+            "price": 3500
         },
         {
             
             "name": "ankylo torso",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "ankylo tail",
-            "price": 10000
+            "price": 2500
         },
         {
             
             "name": "apato skull",
-            "price": 20000
+            "price": 5000
         },
         {
             
             "name": "apato torso",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "apato tail",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "dimetrodon skull",
-            "price": 22000
+            "price": 5500
         },
         {
             
             "name": "dimetrodon torso",
-            "price": 20000
+            "price": 5000
         },
         {
             
             "name": "dimetrodon tail",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "iguanodon skull",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "iguanodon torso",
-            "price": 14000
+            "price": 3500
         },
         {
             
             "name": "iguanodon tail",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "sabertooth skull",
-            "price": 10000
+            "price": 2500
         },
         {
             
             "name": "sabertooth torso",
-            "price": 8000
+            "price": 2000
         },
         {
             
             "name": "pachy skull",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "pachy torso",
-            "price": 14000
+            "price": 3500
         },
         {
             
             "name": "pachy tail",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "parasaur skull",
-            "price": 14000
+            "price": 3500
         },
         {
             
             "name": "parasaur torso",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "parasaur tail",
-            "price": 10000
+            "price": 2500
         },
         {
             
             "name": "diplo skull",
-            "price": 20000
+            "price": 5000
         },
         {
             
             "name": "diplo neck",
-            "price": 20000
+            "price": 5000
         },
         {
             
             "name": "diplo chest",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "diplo hip",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "diplo tail",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "plesio skull",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "plesio neck",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "plesio torso",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "stego skull",
-            "price": 20000
+            "price": 5000
         },
         {
             
             "name": "stego torso",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "stego tail",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "ptera skull",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "ptera left wing",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "ptera right wing",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "ichthyo skull",
-            "price": 10000
+            "price": 2500
         },
         {
             
             "name": "ichthyo torso",
-            "price": 8000
+            "price": 2000
         },
         {
             
             "name": "raptor skull",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "raptor torso",
-            "price": 10000
+            "price": 2500
         },
         {
             
             "name": "styraco skull",
-            "price": 14000
+            "price": 3500
         },
         {
             
             "name": "styraco torso",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "styraco tail",
-            "price": 10000
+            "price": 2500
         },
         {
             
             "name": "spino skull",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "spino torso",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "spino tail",
-            "price": 10000
+            "price": 2000
         },
         {
             
             "name": "megacera skull",
-            "price": 18000
+            "price": 4500
         },
         {
             
             "name": "megacera torso",
-            "price": 14000
+            "price": 3500
         },
         {
             
             "name": "megacera tail",
-            "price": 12000
+            "price": 3000
         },
         {
             
             "name": "archelon skull",
-            "price": 16000
+            "price": 4000
         },
         {
             
             "name": "archelon torso",
-            "price": 14000
+            "price": 3500
         },
         {
             
             "name": "amber",
-            "price": 4800
+            "price": 1200
         },
         {
             
             "name": "ammonite",
-            "price": 4400
+            "price": 1100
         },
         {
             
             "name": "coprolite",
-            "price": 4400
+            "price": 1100
         },
         {
             
             "name": "dinosaur egg",
-            "price": 5600
+            "price": 1400
         },
         {
             
             "name": "fern fossil",
-            "price": 4000
+            "price": 1000
         },
         {
             
             "name": "dinosaur track",
-            "price": 4000
+            "price": 1000
         },
         {
             
             "name": "archaeopteryx",
-            "price": 5200
+            "price": 1300
         },
         {
             
             "name": "peking man",
-            "price": 4400
+            "price": 1100
         },
         {
             
             "name": "shark tooth",
-            "price": 4000
+            "price": 1000
         },
         {
             
             "name": "trilobite",
-            "price": 5200
+            "price": 1300
         },
     ]
