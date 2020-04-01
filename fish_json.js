7d35993df158a4115e4682a8c0ce4c2f82643142
Kyla Burgess <kyla_burgess@outlook.com>
2020-04-01 12:35:29 +1030

    Data update

--- fish_json.js
+++ fish_json.js
@@ -729,7 +729,7 @@ var fish_json =
             "time": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
             "season": {
                 "northern": ["january", "february", "march", "april", "", "", "", "", "", "october", "november", "december"],
-                "southern": ["", "", "", "", "", "", "", "august", "september", "october", "", ""]
+                "southern": ["", "", "", "april", "may", "june", "july", "august", "september", "october", "", ""]
             }
         },
         {
